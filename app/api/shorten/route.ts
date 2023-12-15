fa589fe57416058b380cb46fd09d24007211ff39
TechLion Dev (via GIT) <git@techlion.dev>
2023-12-14 19:48:55 -0500

    FIXED

--- app/api/shorten/route.ts
+++ app/api/shorten/route.ts
@@ -4,7 +4,7 @@ const pb = new PocketBase(url)
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
-    let url = decodeURIComponent(searchParams.get('url') ?? '')
+    let url = searchParams.get('url') ?? ''
     let slug = searchParams.get('slug')
     if (!url) return Response.json({ error: 'No url provided' })
     if (!url.startsWith('http://') && !url.startsWith('https://')) {
